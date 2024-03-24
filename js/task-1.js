ed938ada363c361df1c70238da3980ae9c852f56
Kyryl2 <krasiuk.4884@gmail.com>
2024-03-24 11:57:57 +0100

    hw6 task1

--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,55 @@
-//  Станція з продажу ремонтних дроїдів готова до запуску, залишилося написати програмне забезпечення для відділу продажів.
+// Задача 1. Акаунт користувача
 
-// Оголоси функцію makeTransaction, яка очікує два параметри, значення яких будуть задаватися під час її виклику: • quantity— перший параметр, число, що містить кількість замовлених дроїдів • pricePerDroid — другий параметр, число, що містить вартість одного дроїда
+// Виконуй це завдання у файлі task-1.js
 
-// Доповни код функції так, щоб вона повертала рядок з повідомленням про покупку ремонтних дроїдів: "You ordered <quantity> droids worth <totalPrice> credits!", де: • <quantity> — це кількість замовлених дроїдів • <totalPrice> — це загальна вартість замовлення, тобто вартість усіх замовлених дроїдів
+// Перед звільненням розробник зламав вихідний код управління акаунтами користувачів нашого сервісу доставки їжі. Виконай рефакторинг методів об'єкта customer, розставивши відсутні this під час звернення до властивостей об'єкта.
 
-// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+// Використай цей стартовий код і виконай рефакторинг. Після оголошення об'єкта ми додали виклики методів. У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
 
-function makeTransaction(quantity, pricePerDroid) {
-  const totalPrice = Number(quantity) * Number(pricePerDroid);
-  return `"You ordered ${quantity} droids worth ${totalPrice} credits!"`;
-}
+const customer = {
+  username: 'Mango',
+  balance: 24000,
+  discount: 0.1,
+  orders: ['Burger', 'Pizza', 'Salad'],
+  // Change code below this line
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  },
+  // Change code above this line
+};
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, 'Steak');
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+
+// Залиш цей код для перевірки ментором.
+
+// На що буде звертати увагу ментор при перевірці:
+
+// Оголошена змінна customer
+// Значення змінної customer — це об'єкт із властивостями та методами
+// Виклик customer.getDiscount() повертає поточне значення властивості discount
+// Виклик customer.setDiscount(0.15) оновлює значення властивості discount
+// Виклик customer.getBalance() повертає поточне значення властивості balance.
+// Виклик customer.getOrders() повертає поточне значення властивості orders
+// Виклик customer.addOrder(5000, "Steak") додає "Steak" у масив значень властивості orders та оновлює баланс
+// Метод getBalance об'єкта customer використовує this
+// Метод getDiscount об'єкта customer використовує this
+// Метод setDiscount об'єкта customer використовує this
+// Метод getOrders об'єкта customer використовує this
+// Метод addOrder об'єкта customer використовує this
